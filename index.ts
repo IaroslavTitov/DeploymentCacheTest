676eb045ded57bb893f4aeec890a1ab2c82653e3
IaroslavTitov <iaro@pulumi.com>
2024-12-10 15:18:23 -0700

    test

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
+import { Secret } from "@pulumi/aws/secretsmanager";
 import * as pulumi from "@pulumi/pulumi";
+import { Output } from "@pulumi/pulumi";
 import * as service from "@pulumi/pulumiservice";
 
-const serviceOrg = "service-provider-test-org";
+const serviceOrg = "yoba";
 let config = new pulumi.Config();
 
-const team = new service.Team("cool team", {
-  organizationName: "yoba",
-  teamType: "pulumi",
-  name: "coolteam",
-});
+const stack = new service.Stack("my_stack", {
+  organizationName: serviceOrg,
+  projectName: "my-test-project",
+  stackName: "AgentPoolTest",
+})
+
+const pulumi_agent_pool = new service.AgentPool("agentPoolResource", {
+  name: "agent-pool-resource",
+  organizationName: serviceOrg,
+  description: "this is to test agent pool id",
+})
