6e676bf04b0822b32915ec3563beba2d2ca4e98b
IaroslavTitov <iaro@pulumi.com>
2024-12-10 15:34:47 -0700

    a

--- index.ts
+++ index.ts
@@ -3,17 +3,11 @@ import * as pulumi from "@pulumi/pulumi";
 import { Output } from "@pulumi/pulumi";
 import * as service from "@pulumi/pulumiservice";
 
-const serviceOrg = "yoba";
+const serviceOrg = "boba";
 let config = new pulumi.Config();
 
 const stack = new service.Stack("my_stack", {
   organizationName: serviceOrg,
-  projectName: "my-test-project",
-  stackName: "AgentPoolTest",
-})
-
-const pulumi_agent_pool = new service.AgentPool("agentPoolResource", {
-  name: "agent-pool-resource",
-  organizationName: serviceOrg,
-  description: "this is to test agent pool id",
+  projectName: "dummy",
+  stackName: "dummy",
 })
